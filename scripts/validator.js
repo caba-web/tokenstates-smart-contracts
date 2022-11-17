23049bbb91950c89fdf7d649466976ed5573d278
aleks <alekssurikov2201@yandex.ru>
2022-11-17 15:59:46 +0300

    feat: deploy mainnet validator

--- scripts/validator.js
+++ scripts/validator.js
@@ -29,16 +29,16 @@ async function main(proxyRouterAddress = "0x000000000000000000000000000000000000
 	console.log(await (await hre.ethers.getSigner()).getBalance())
 
 	// Base deploy immutable
-    // const Validator = await hre.ethers.getContractFactory("Validator");
-    // const validator = await Validator.deploy(...args);
+    const Validator = await hre.ethers.getContractFactory("Validator");
+    const validator = await Validator.deploy(...args);
 
     // Deploying mutable
     // const Validator = await hre.ethers.getContractFactory("ValidatorUpgradeable");
     // const validator = await upgrades.deployProxy(Validator, args, {kind: "uups"});
 
     // Upgrading
-    const Validator = await ethers.getContractFactory("ValidatorUpgradeable");
-    const validator = await upgrades.upgradeProxy("0x7c70c6740c2715a03c329b0503bc935d424d3166", Validator);
+    // const Validator = await ethers.getContractFactory("ValidatorUpgradeable");
+    // const validator = await upgrades.upgradeProxy("0x7c70c6740c2715a03c329b0503bc935d424d3166", Validator);
 
 
 	await validator.deployed();
@@ -49,7 +49,7 @@ async function main(proxyRouterAddress = "0x000000000000000000000000000000000000
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
-main("0x5f08c8D3Cd8Ee8FF60bBB39a5310bd1384bc427A")
+main("0x071a03D627B6989680479290610A6e48b96f7f80")
 	.then(() => process.exit(0))
 	.catch((error) => {
 		console.error(error);
